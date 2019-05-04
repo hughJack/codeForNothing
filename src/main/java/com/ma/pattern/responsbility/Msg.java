cac7e399f7c60b58b2bd489b92da00fec08b2e15
hughJack <913065123@qq.com>
2019-05-04 21:50:38 +0800

    二代责任链:  改进 : 链式编程

--- src/main/java/com/ma/pattern/responsbility/Msg.java
+++ src/main/java/com/ma/pattern/responsbility/Msg.java
@@ -17,14 +17,18 @@ public class Msg {
 
   public static void main(String[] args) {
     Msg msg = new Msg();
-    msg.setMsg("Hello <world>, 996 bad guy");
+    msg.setMsg("Hello world,<script>,:),996 bad guy from http://www.github.com");
     msg.setName("yangguan");
-    // 责任链模式,, 形成一个链条??
-    FilerChain filerChain = new FilerChain();
-    filerChain.add(new HtmlFilter());
-    filerChain.add(new SenstiveFilter());
-    filerChain.doFilter(msg);
-    //
+    // 责任链1
+    FilterChain filterChain = new FilterChain();
+    // 链式编程???
+    filterChain.add(new HtmlFilter()).add(new SenstiveFilter());
+    // 责任链2
+    FilterChain filterChain2 = new FilterChain();
+    filterChain2.add(new FaceFilter()).add(new UrlFilter());
+    filterChain.add(filterChain2);
+    // 遍历责任链
+    filterChain.doFilter(msg);
     System.out.println(msg);
   }
 
@@ -66,16 +70,38 @@ class SenstiveFilter implements Filter {
   }
 }
 
+class FaceFilter implements Filter {
+
+  @Override
+  public void doFilter(Msg msg) {
+    String replace = msg.getMsg();
+    replace = replace.replaceAll(":", "V");
+    msg.setMsg(replace);
+  }
+}
+
+class UrlFilter implements Filter {
+
+  @Override
+  public void doFilter(Msg msg) {
+    String replace = msg.getMsg();
+    replace = replace.replaceAll("http", "https");
+    msg.setMsg(replace);
+  }
+}
 
 @Data
-class FilerChain {
+class FilterChain implements Filter {
 
   List<Filter> filters = new ArrayList<>();
 
-  public void add(Filter f) {
+  // 如果实现了Filter接口, 那么也可以实现添加  另外一个链条, 因为List支持
+  public FilterChain add(Filter f) {
     filters.add(f);
+    return this;
   }
 
+  @Override
   public void doFilter(Msg msg) {
     for (Filter filter : filters) {
       filter.doFilter(msg);
